6fd03c361b6905c19413f83e2d8b9d8778bef4eb
perawat_tammawong <p.tummawong@outlook.com>
2020-04-27 17:33:29 +0700

    update routh

--- local/resources/views/authen_office_365/authen_office.blade.php
+++ local/resources/views/authen_office_365/authen_office.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Quickstart | MSAL.JS Vanilla JavaScript SPA</title>
-     
+
     <!-- msal.js with a fallback to backup CDN -->
     <script type="text/javascript" src="https://alcdn.msauth.net/lib/1.2.1/js/msal.js" integrity="sha384-9TV1245fz+BaI+VvCjMYL0YDMElLBwNS84v3mY57pXNOt6xcUYch2QLImaTahcOP" crossorigin="anonymous"></script>
     <script type="text/javascript">
@@ -56,17 +56,17 @@
 
     <!-- importing bootstrap.js and supporting js libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
+
     <!-- importing app scripts (load order is important) -->
-    <script type="text/javascript" src="./authConfig.js"></script>
-    <script type="text/javascript" src="./graphConfig.js"></script>
-    <script type="text/javascript" src="./ui.js"></script>
-    
+    <script type="text/javascript" src="{{ url('/authConfig.js') }}"></script>
+    <script type="text/javascript" src="{{ url('/graphConfig.js') }}"></script>
+    <script type="text/javascript" src="{{ url('/ui.js') }}"></script>
+
     <!-- replace next line with authRedirect.js if you would like to use the redirect flow -->
     <!-- <script type="text/javascript" src="./authRedirect.js"></script>   -->
-    <script type="text/javascript" src="./authPopup.js"></script>
-    <script type="text/javascript" src="./graph.js"></script>
+    <script type="text/javascript" src="{{ url('/authPopup.js') }}"></script>
+    <script type="text/javascript" src="{{ url('/graph.js') }}"></script>
   </body>
 </html>
