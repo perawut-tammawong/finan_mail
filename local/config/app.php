b525bf1978706b234fdc247471f2540b82f47e53
perawat_tammawong <p.tummawong@outlook.com>
2020-05-01 17:01:34 +0700

    update new laravel

--- local/config/app.php
+++ local/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/finan_mail'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- local/routes/web.php
+++ local/routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    //return view('authen_office_365.authen_office');
-    //return view('authen_office_365.testma');
-    return view('admin.home');
+    return view('welcome');
 });
