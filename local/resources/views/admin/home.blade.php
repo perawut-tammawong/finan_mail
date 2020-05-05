ee089d403bbbf3eb15608028332d9eb9f0f7aa8f
perawat_tammawong <p.tummawong@outlook.com>
2020-05-05 11:28:17 +0700

    update

--- local/resources/views/admin/home.blade.php
+++ local/resources/views/admin/home.blade.php
@@ -170,50 +170,7 @@ to get the desired effect
 
       <!-- Small Box (Stat card) -->
       <div class="row">
-        <div class="col-lg-3 col-6">
-          <!-- small card -->
-          <div class="small-box bg-info">
-            <!-- Loading (remove the following to stop the loading)-->
-            <div class="overlay">
-              <i class="fas fa-3x fa-sync-alt"></i>
-            </div>
-            <!-- end loading -->
-            <div class="inner">
-              <h3>150</h3>
 
-              <p>New Orders</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-shopping-cart"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-              More info <i class="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small card -->
-          <div class="small-box bg-success">
-            <!-- Loading (remove the following to stop the loading)-->
-            <div class="overlay dark">
-              <i class="fas fa-3x fa-sync-alt"></i>
-            </div>
-            <!-- end loading -->
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-              More info <i class="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
-        </div>
-        <!-- ./col -->
       </div>
       <!-- /.row -->
     </div>
